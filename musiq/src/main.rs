9198709bd863546c797f3ff63097cc144d46b0df
Semih Yonet <semihyonet@gmail.com>
2025-04-09 00:57:49 +0200

    Create a testable base

--- musiq/src/main.rs
+++ musiq/src/main.rs
@@ -1,3 +1,14 @@
+use musiqlang::lexer;
+use musiqlang::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    let content = std::fs::read_to_string(path).expect("Failed to read file");
+
+    let tokens = lexer::tokenize(&content);
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse();
+
+    println!("{:#?}", ast);
 }
